238134de9fd7d5916793ca1336b16932211bc0ba
oenegm <oenegm@gmail.com>
2023-08-25 01:05:31 +0300

    init

---
+++ src/main/kotlin/com/project/vehiclerenterkotlin/VehicleRenterKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.project.vehiclerenterkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VehicleRenterKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<VehicleRenterKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/project/vehiclerenterkotlin/VehicleRenterKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.project.vehiclerenterkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VehicleRenterKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
